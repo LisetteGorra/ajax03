1f957c1b9c9549470ed6dca55d6c0f8e7265caef
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-05 18:34:57 +0000

    POST request, setRequestHeader, application/x-ww-form-urlencoded, set params in ajax04 file

--- process.php
+++ process.php
@@ -2,7 +2,13 @@
 
 echo 'Processing...';
 
+//check for a POST variable
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '. $_POST['name'];
+}
+
 //check for a GET variable
 if(isset($_GET['name'])){
     echo 'GET: Your name is '. $_GET['name'];
 }
+
