a3d18758a6998d3599dabe298238cb329b49cc8f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-05 19:08:15 +0000

    fetched data from php, mysql, 1 button, GET users

---
+++ users.php
@@ -0,0 +1,14 @@
+<?php
+//Create Connection
+$conn = mysqli_connect('localhost', 'root', '123456', 'ajaxtest');
+
+$query = 'SELECT * FROM users';
+
+//GET result 
+$result = mysqli_query($conn, $query);
+
+//Fetch Data
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($users);
+// this takes our result and puts the array into json format and gives us our response
